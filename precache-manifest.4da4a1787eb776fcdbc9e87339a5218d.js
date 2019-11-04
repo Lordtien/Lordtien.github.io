2adb4e585b82461c3622e3544f2fbc1379e5a8da
lordtien <nitinpawarme@gmail.com>
2019-11-04 07:23:17 +0530

    Updates

---
+++ precache-manifest.4da4a1787eb776fcdbc9e87339a5218d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca007699af6e46bb84c873bb14af34d",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "be6d3647b8087f093e8e",
+    "url": "/snake/static/css/main.99774ef6.chunk.css"
+  },
+  {
+    "revision": "7235a43a298f19061221",
+    "url": "/snake/static/js/2.0ae2a175.chunk.js"
+  },
+  {
+    "revision": "be6d3647b8087f093e8e",
+    "url": "/snake/static/js/main.3eb3beb5.chunk.js"
+  },
+  {
+    "revision": "c6840efb4dea3afa7c47",
+    "url": "/snake/static/js/runtime-main.ecfa016f.js"
+  }
+]);
